50c8a5e1c7771802fe4ff0e5c7773cd156d08166
TranDatt-labo <23010128@st.phenikaa-uni.edu.vn>
2026-06-07 20:07:06 +0700

    t

--- t/unit/examples/test_distributed_features_skeleton.py
+++ t/unit/examples/test_distributed_features_skeleton.py
@@ -1,12 +1,10 @@
 from examples.distributed_features.app import app
 from examples.distributed_features.tasks import add, hello
 
-
 def test_demo_app_uses_redis_defaults():
     assert app.main == 'distributed_features'
     assert app.conf.broker_url == 'redis://localhost:6379/0'
     assert app.conf.result_backend == 'redis://localhost:6379/1'
-
 
 def test_demo_tasks_are_registered():
     assert add.name == 'distributed_features.add'
